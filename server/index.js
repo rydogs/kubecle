e91484afe32de551993c722d707ffc7f81ef300b
Roderick Rodriguez <rod@rodrod.com>
2018-07-08 20:49:03 -0400

    Updated routes to use res.json()

--- server/index.js
+++ server/index.js
@@ -10,40 +10,34 @@ app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/api/namespace/', asyncHandler(async (req, res) => {
   const deployments = await client.apis.apps.v1.namespaces().get();
-  res.setHeader('Content-Type', 'application/json');
-  res.send(deployments);
+  res.json(deployments);
 }));
 
 app.get('/api/namespace/:namespace/deployments', asyncHandler(async (req, res) => {
   const deployments = await client.apis.apps.v1.namespaces(req.params.namespace).deployments().get();
-  res.setHeader('Content-Type', 'application/json');
-  res.send(deployments);
+  res.json(deployments);
 }));
 
 app.get('/api/namespace/:namespace/pods', asyncHandler(async (req, res) => {
-  let pods = await client.api.v1.namespaces(req.params.namespace).pods().get();
-  res.setHeader('Content-Type', 'application/json');
-  res.send(pods);
+  const pods = await client.api.v1.namespaces(req.params.namespace).pods().get();
+  res.json(pods);
 }));
 
 app.get('/api/namespace/:namespace/pods/:pods/logs/:containerName?', asyncHandler(async (req, res) => {
-  let logs = await client.api.v1.namespaces(req.params.namespace).pods(req.params.pods).log.get({
+  const logs = await client.api.v1.namespaces(req.params.namespace).pods(req.params.pods).log.get({
     qs: { container: req.params.containerName }
   });
-  res.setHeader('Content-Type', 'application/text');
-  res.send(logs.body);
+  res.json(logs.body);
 }));
 
 app.delete('/api/namespace/:namespace/pods/:podname', asyncHandler(async (req, res) => {
-  let pods = await client.api.v1.namespaces(req.params.namespace).pods(req.params.podname).delete();
-  res.setHeader('Content-Type', 'application/json');
-  res.send(pods);
+  const pods = await client.api.v1.namespaces(req.params.namespace).pods(req.params.podname).delete();
+  res.json(pods);
 }));
 
 app.get('/api/namespace/:namespace/services', asyncHandler(async (req, res) => {
-  let pods = await client.api.v1.namespaces(req.params.namespace).services().get();
-  res.setHeader('Content-Type', 'application/json');
-  res.send(pods);
+  const pods = await client.api.v1.namespaces(req.params.namespace).services().get();
+  res.json(pods);
 }));
 
 var server = app.listen(process.env.PORT || 6888, async () => {
