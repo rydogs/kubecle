cc8c903e83cf807b1c804d16a0e5a57f5e462fd0
Ryan Liu <ryanyuliu@gmail.com>
2019-01-14 16:01:38 -0500

    Support drag, copy and paste in electron

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
-const { app, BrowserWindow } = require('electron')
-const { server } = require("./server")
+const { app, BrowserWindow, Menu} = require('electron');
+const { server } = require("./server");
+var mainWindow = null;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
-
-function createWindow () {
-  let win = new BrowserWindow({ width: 1400, height: 1000, frame: false })
-  win.loadURL('http://localhost:6888');
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    win = null
-  });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function(){
     app.quit();
-  }
-})
+});
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
-})
+app.on("ready", function () {
+    mainWindow = new BrowserWindow({ width: 1400, height: 1000, frame: false })
+    mainWindow.loadURL('http://localhost:6888');
+    mainWindow.on("closed", function () {
+        mainWindow =  null;
+    });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+    // Create the Application's main menu
+    var template = [{
+        label: "Kubecle",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+});
