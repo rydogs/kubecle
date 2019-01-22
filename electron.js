671f6fec637c343154d424c16708eeb8a2fd7bcc
Ryan Liu <ryanyuliu@gmail.com>
2019-01-21 23:02:50 -0500

    Check for update and open github latest release page

--- electron.js
+++ electron.js
@@ -1,22 +1,23 @@
-const { app, BrowserWindow, Menu} = require('electron');
+const { app, BrowserWindow, Menu, dialog, shell} = require('electron');
+const { autoUpdater } = require('electron-updater');
 const { server } = require("./server");
-var mainWindow = null;
 
+var mainWindow = null;
 app.on("window-all-closed", function(){
     app.quit();
 });
 
 app.on("ready", function () {
-    mainWindow = new BrowserWindow({ width: 1400, height: 1000, frame: false })
+    mainWindow = new BrowserWindow({ width: 1400, height: 1000, frame: false });
     mainWindow.loadURL('http://localhost:6888');
     mainWindow.on("closed", function () {
         mainWindow =  null;
     });
-
     // Create the Application's main menu
     var template = [{
         label: "Kubecle",
         submenu: [
+            { label: "Check for Update", click: checkForUpdates },
             { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
             { type: "separator" },
             { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
@@ -35,3 +36,48 @@ app.on("ready", function () {
 
     Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
+
+autoUpdater.autoDownload = false;
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+var appReadyCheck = true;
+
+app.on("ready", function () {
+    autoUpdater.checkForUpdates();
+});
+
+autoUpdater.on('update-available', info => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Found Updates',
+        message: `New version ${info.releaseName} found, do you download it from github?`,
+        buttons: ['Sure', 'No']
+        }, (buttonIndex) => {
+        if (buttonIndex === 0) {
+            shell.openExternal('https://github.com/rydogs/kubecle/releases/latest');
+            updater.enabled = true;
+        } else {
+            updater.enabled = true;
+            updater = null;
+        };
+    });
+});
+
+autoUpdater.on('update-not-available', () => {
+    if (!appReadyCheck) {
+        dialog.showMessageBox({
+            type: 'info',
+            title: 'Update not available',
+            message: `Update not available, v${app.getVersion()} is the latest version.`,
+            buttons: ['Close']
+        });
+    } else {
+        appReadyCheck = false;
+    }
+});
+
+function checkForUpdates (menuItem, focusedWindow, event) {
+    updater = menuItem;
+    updater.enabled = false;
+    autoUpdater.checkForUpdates();
+}
